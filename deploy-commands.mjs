014f920033030d00f6ea87b505cfeb067730c1a0
Oskar Risberg <oskar.risberg@stendahls.se>
2021-12-29 15:31:51 +0100

    add deploy-commands script

---
+++ deploy-commands.mjs
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+import {REST} from '@discordjs/rest';
+import {Routes} from 'discord-api-types/v9';
+
+const { clientId, guildId, token } = JSON.parse(fs.readFileSync('config.json'));
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.mjs'));
+
+for (const file of commandFiles) {
+	const command = await import(`./commands/${file}`);
+	commands.push(command.default.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
+
+/*
+
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
+*/
