67670773c2524b7feddfa457a8dd082210dfc6be
Oskar Risberg <oskar.risberg@stendahls.se>
2021-12-29 11:54:34 +0100

    clarify help command

--- commands/help.mjs
+++ commands/help.mjs
@@ -5,11 +5,31 @@ import {
 const help = (message) => {
     const helpCommand = message.content.toLowerCase().replace('!help', '').trim();
     const commands = {
-        'help': { syntax: '!help [{command}]', description: 'Show details for {command}', examples: '!help rig' },
-        'map': { syntax: '!map [{mapname} {mapname}] [-{mapname}]', description: 'Select a random map', examples: '!map -woods\r\n!map customs shoreline interchange' },
-        'price': { syntax: '!price {itemname}', description: 'Show prices for item(s) matching {itemname}', examples: '!price bitcoin' },
-        'barter': { syntax: '!barter {itemname}', description: 'Shows barter trades for item(s) matching {itemname}', examples: '!barter slick' },
-        'craft': { syntax: '!craft {itemname}', description: 'Shows crafts for item(s) matching {itemname}', examples: '!craft 7n31' }
+        'help': {
+            syntax: '!help [command]',
+            description: 'Show details for command',
+            examples: '!help barter',
+        },
+        'map': {
+            syntax: '!map [mapname mapname] [-mapname]',
+            description: 'Select a random map',
+            examples: '!map -woods\r\n!map customs shoreline interchange',
+         },
+        'price': {
+            syntax: '!price itemname',
+            description: 'Show prices for item(s) matching itemname',
+            examples: '!price bitcoin',
+        },
+        'barter': {
+            syntax: '!barter itemname',
+            description: 'Shows barter trades for item(s) matching itemname',
+            examples: '!barter slick',
+        },
+        'craft': {
+            syntax: '!craft itemname',
+            description: 'Shows crafts for item(s) matching itemname',
+            examples: '!craft 7n31',
+        },
     };
     const embed = new MessageEmbed();
 
@@ -23,7 +43,7 @@ const help = (message) => {
         if (commands.hasOwnProperty(helpCommand)) {
             const c = commands[helpCommand];
             embed.setTitle("!" + helpCommand + " command help");
-            embed.addField(c.syntax, c.description + "\r\nExamples:\r\n" + c.examples);
+            embed.addField(c.syntax, c.description + "\r\n\r\nExamples:\r\n" + c.examples);
         }
     }
 
