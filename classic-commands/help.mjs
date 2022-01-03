2331b92d0876e3d2933acb1d1eb8352f6adfc78b
Oskar Risberg <oskar.risberg@stendahls.se>
2022-01-03 09:35:58 +0100

    show we have some slash commands in help

--- classic-commands/help.mjs
+++ classic-commands/help.mjs
@@ -12,22 +12,22 @@ const help = (message) => {
             examples: '!help barter',
         },
         'map': {
-            syntax: '!map [mapname mapname] [-mapname]',
+            syntax: '/map or !map [mapname mapname] [-mapname]',
             description: 'Select a random map',
             examples: '!map -woods\r\n!map customs shoreline interchange',
          },
         'price': {
-            syntax: '!price itemname',
+            syntax: '/price or !price itemname',
             description: 'Show prices for item(s) matching itemname',
             examples: '!price bitcoin',
         },
         'barter': {
-            syntax: '!barter itemname',
+            syntax: '/barter or !barter itemname',
             description: 'Shows barter trades for item(s) matching itemname',
             examples: '!barter slick',
         },
         'craft': {
-            syntax: '!craft itemname',
+            syntax: '/craft or !craft itemname',
             description: 'Shows crafts for item(s) matching itemname',
             examples: '!craft 7n31',
         },
