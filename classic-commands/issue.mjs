9f521cc5e935ceecb4da690f4047e1e171e69ccf
Oskar Risberg <oskar.risberg@stendahls.se>
2022-01-06 12:10:08 +0100

    Make issue reports send to a pre-defined channel

--- classic-commands/issue.mjs
+++ classic-commands/issue.mjs
@@ -2,14 +2,44 @@ import {
     MessageEmbed,
 } from 'discord.js';
 
-const issue = (message, client) => {
+const issue = async (message, client) => {
 	const details = message.content.toLowerCase().replace('!issue ', '');
+    const sendTo = message.fallbackChannel || message.channel;
+
     const embed = new MessageEmbed();
+
+    let reportTo = await client.users.fetch(process.env.ADMIN_ID, false);
+
+    if (client.guilds.cache.has(process.env.ISSUE_SERVER_ID)) {
+        const server = client.guilds.cache.get(process.env.ISSUE_SERVER_ID);
+        const reportingChannel = server.channels.cache.get(process.env.ISSUE_CHANNEL_ID);
+
+        if(reportingChannel){
+            reportTo = reportingChannel;
+        }
+    }
+
 	embed.setTitle("New Issue Reported 🐞");
 	embed.setDescription(`**Issue Description:**\n${details}`);
-	embed.setFooter(`This issue was reported by @${message.author.username} | Server: ${message.guild.name}`);
+    let footerText = `This issue was reported by @${message.author.username}`;
 
-	message.channel.send({embeds: [embed]});
+    if(message.guild){
+        footerText = `${footerText} | Server: ${message.guild.name}`;
+    } else {
+        footerText = `${footerText} | Reported in a DM`;
+    }
+
+	embed.setFooter({
+        text: footerText,
+    });
+
+	reportTo.send({
+        embeds: [embed],
+    });
+
+    sendTo.send({
+        content: `Thanks for reporting, we're on it!`,
+    });
 };
 
 export default issue;
