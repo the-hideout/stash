f6760f816d4b02c599b1063759cdc659f8068e39
Oskar Risberg <oskar.risberg@stendahls.se>
2022-01-08 23:37:09 +0100

    handle no messages

--- commands/status.mjs
+++ commands/status.mjs
@@ -63,7 +63,7 @@ const defaultFunction = {
 
         embed.setTitle(globalStatus.message);
         embed.setURL('https://status.escapefromtarkov.com/');
-        embed.setDescription(currentStatus.messages[0].content);
+        embed.setDescription(currentStatus.messages[0]?.content);
         // embed.setAuthor({
         //     name: 'Built by tarkov-tools',
         //     iconURL: 'https://tarkov-tools.com/apple-touch-icon.png',
