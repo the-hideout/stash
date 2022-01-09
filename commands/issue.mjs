82e90e0792249037290e8fac1b03b96eda168a11
Oskar Risberg <oskar.risberg@stendahls.se>
2022-01-09 15:57:21 +0100

    Separate send error into it's own module. Closes #17

--- commands/issue.mjs
+++ commands/issue.mjs
@@ -1,7 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import {
-    MessageEmbed,
-} from "discord.js";
+
+import sendError from '../modules/send-error.mjs';
 
 const defaultFunction = {
     data: new SlashCommandBuilder()
@@ -16,39 +15,7 @@ const defaultFunction = {
         const { client, member } = interaction;
         const details = interaction.options.getString("message");
 
-        const embed = new MessageEmbed();
-
-        let reportTo = await client.users.fetch(process.env.ADMIN_ID, false);
-
-
-        if (client.guilds.cache.has(process.env.ISSUE_SERVER_ID)) {
-            const server = client.guilds.cache.get(process.env.ISSUE_SERVER_ID);
-            const reportingChannel = server.channels.cache.get(process.env.ISSUE_CHANNEL_ID);
-
-            if (reportingChannel) {
-                reportTo = reportingChannel;
-            }
-        }
-
-        embed.setTitle("New Issue Reported 🐞");
-        embed.setDescription(`**Issue Description:**\n${details}`);
-
-
-        let footerText = `This issue was reported by @${member.user.username}`;
-
-        if (member.guild) {
-            footerText = `${footerText} | Server: ${member.guild.name}`;
-        } else {
-            footerText = `${footerText} | Reported in a DM`;
-        }
-
-        embed.setFooter({
-            text: footerText,
-        });
-
-        reportTo.send({
-            embeds: [embed],
-        });
+        sendError(client, member, details);
 
         interaction.reply({
             content: "Thanks for reporting, we're on it!",
