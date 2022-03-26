9aa06ae8109a0ae205b7c592b87777b37965fae4
Grant Birkinbine <grantbirki@github.com>
2022-03-25 21:34:27 -0700

    add helper class to send general error messages in an embed

---
+++ modules/general-error.mjs
@@ -0,0 +1,24 @@
+import {
+    MessageEmbed,
+} from 'discord.js';
+
+// Send a generic error message as an embed
+// :param interaction: The Discord interaction object
+// :param message: The message text to send
+const generalError = async (interaction, message) => {
+    const sendTo = interaction.fallbackChannel || interaction.channel;
+    const embed = new MessageEmbed();
+
+    message = message || 'An error has occurred';
+    message = `${interaction.user} ${message}`;
+
+    embed.setTitle('❌ ERROR ❌');
+    embed.setDescription(message);
+
+    sendTo.send({
+        embeds: [embed],
+    });
+};
+
+export default generalError;
+
