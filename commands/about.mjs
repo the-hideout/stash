3940329127aca28b093c398308398aa84d478e69
Grant Birkinbine <grantbirki@github.com>
2022-03-31 23:23:40 -0600

    add status page field

--- commands/about.mjs
+++ commands/about.mjs
@@ -32,6 +32,7 @@ const defaultFunction = {
         });
         embed.addField('Bugs? Missing features? Chat with us on discord!', 'https://discord.gg/XPAsKGHSzH', true);
         embed.addField('Want to contribute to the bot or checkout the source code? View the project on GitHub!', 'https://github.com/the-hideout/stash', true);
+        embed.addField('Want to check the status of our services (api, website, bot, etc)?', 'https://status.tarkov.dev', true);
         // embed.addField('Like it? Support on Patreon', 'https://www.patreon.com/kokarn', true);
         embed.setFooter({
             text: 'Enjoy ❤️',
