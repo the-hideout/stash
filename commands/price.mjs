24ebee311fc86afdc79944c2e7dd7f8b75f7608d
Grant Birkinbine <grantbirki@github.com>
2022-04-01 09:30:46 -0600

    last updated relative time

--- commands/price.mjs
+++ commands/price.mjs
@@ -55,7 +55,7 @@ const defaultFunction = {
             let body = "**Price and Item Details:**\n";
             embed.setTitle(item.name);
             embed.setURL(item.link);
-            embed.setFooter({text: `Last Updated: ${moment(item.updated).format('MMMM Do YYYY, h:mm:ss a')} UTC`});
+            embed.setFooter({text: `Last Updated: ${moment(item.updated).fromNow()}`});
 
 
             if (item.iconLink) {
