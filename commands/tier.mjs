12af8d61a329c19bfd1758f84c3e2cc9a904775f
Blightbuster <moritz.scheve@gmx.de>
2022-04-18 00:26:31 +0200

    Fix indentation

--- commands/tier.mjs
+++ commands/tier.mjs
@@ -12,17 +12,17 @@ const defaultFunction = {
         .setDescription('Shows the criteria for loot tiers'),
 
     async execute(interaction) {
-		const tiers = getTiers();
+        const tiers = getTiers();
         const embed = new MessageEmbed()
-        	.setTitle("Loot Tiers")
-        	.setDescription(`
-				Loot tiers are divided primarily by the per-slot value of the item:
-				• ${lootTier(tiers.legendary).msg} >= ${tiers.legendary.toLocaleString()}₽
-				• ${lootTier(0, true).msg}
-				• ${lootTier(tiers.great).msg} >= ${tiers.great.toLocaleString()}₽
-				• ${lootTier(tiers.average).msg} >= ${tiers.average.toLocaleString()}₽
-				• ${lootTier(tiers.average -1).msg} < ${tiers.average.toLocaleString()}₽
-			`);
+            .setTitle("Loot Tiers")
+            .setDescription(`
+                Loot tiers are divided primarily by the per-slot value of the item:
+                • ${lootTier(tiers.legendary).msg} >= ${tiers.legendary.toLocaleString()}₽
+                • ${lootTier(0, true).msg}
+                • ${lootTier(tiers.great).msg} >= ${tiers.great.toLocaleString()}₽
+                • ${lootTier(tiers.average).msg} >= ${tiers.average.toLocaleString()}₽
+                • ${lootTier(tiers.average -1).msg} < ${tiers.average.toLocaleString()}₽
+            `);
 
         await interaction.editReply({ embeds: [embed] });
     },
