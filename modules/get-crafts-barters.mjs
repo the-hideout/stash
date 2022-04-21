46b6f448756606c7b3d6cabeea43111a413cfcc6
Grant Birkinbine <grant.birkinbine@gmail.com>
2022-04-20 21:44:40 -0400

    format the query

--- modules/get-crafts-barters.mjs
+++ modules/get-crafts-barters.mjs
@@ -2,9 +2,73 @@ import graphqlRequest from "./graphql-request.mjs";
 
 const getCraftsBarters = async () => {
     const query = `query {
-        crafts { source duration requiredItems { item { id name iconLink avg24hPrice lastLowPrice buyFor { source price currency requirements { type value } } } count attributes { type value } } rewardItems { item { id name iconLink link } count } }
-        barters { source requiredItems { item { id name avg24hPrice lastLowPrice buyFor { source price currency requirements { type value } } } count } rewardItems { item { id name iconLink link } count } }
-    }`;
+        crafts {
+          source
+          duration
+          requiredItems {
+            item {
+              id
+              name
+              iconLink
+              avg24hPrice
+              lastLowPrice
+              buyFor {
+                source
+                price
+                currency
+                requirements {
+                  type
+                  value
+                }
+              }
+            }
+            count
+            attributes {
+              type
+              value
+            }
+          }
+          rewardItems {
+            item {
+              id
+              name
+              iconLink
+              link
+            }
+            count
+          }
+        }
+        barters {
+          source
+          requiredItems {
+            item {
+              id
+              name
+              avg24hPrice
+              lastLowPrice
+              buyFor {
+                source
+                price
+                currency
+                requirements {
+                  type
+                  value
+                }
+              }
+            }
+            count
+          }
+          rewardItems {
+            item {
+              id
+              name
+              iconLink
+              link
+            }
+            count
+          }
+        }
+      }`;
     const response = await graphqlRequest({ graphql: query });
 
     return {
