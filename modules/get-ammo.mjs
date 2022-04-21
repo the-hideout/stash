03dc761000f4e6ccbf917fd642adfc29944520da
Grant Birkinbine <grant.birkinbine@gmail.com>
2022-04-20 21:45:13 -0400

    format another query

--- modules/get-ammo.mjs
+++ modules/get-ammo.mjs
@@ -2,8 +2,21 @@ import graphqlRequest from "./graphql-request.mjs";
 
 const getAmmo = async () => {
     const query = `query {
-        ammo { item { id name shortName iconLink } caliber penetrationPower damage armorDamage fragmentationChance initialSpeed }
-    }`;
+        ammo {
+          item {
+            id
+            name
+            shortName
+            iconLink
+          }
+          caliber
+          penetrationPower
+          damage
+          armorDamage
+          fragmentationChance
+          initialSpeed
+        }
+      }`;
     const response = await graphqlRequest({ graphql: query });
     return response.data.ammo;
 };
