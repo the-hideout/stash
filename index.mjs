7b095ddffc37564ddff05553364743c197f5663d
Razzmatazz <35779878+Razzmatazzz@users.noreply.github.com>
2022-07-21 07:55:20 -0500

    Update index.mjs
    Instead of disabling entirely, we can disable only for production. The online notifications are useful in development to know when the bot is online and ready to receive commands for testing.

--- index.mjs
+++ index.mjs
@@ -62,13 +62,13 @@ discordClient.on('ready', () => {
 
     console.log(message);
 
-    // Send a DM to the admins that the bot is online
-    // if (process.env.ADMIN_ID) {
-    //     discordClient.users.fetch(process.env.ADMIN_ID.split(',')[0], false)
-    //         .then(user => {
-    //             user.send(message);
-    //         });
-    // }
+    // Send a DM to the admin that the bot is online for testing
+    if (process.env.ADMIN_ID && process.env.NODE_ENV !== 'production') {
+        discordClient.users.fetch(process.env.ADMIN_ID.split(',')[0], false)
+            .then(user => {
+                user.send(message);
+            });
+    }
 
     discordClient.user.setActivity('Tarkov.dev', {
         type: 'PLAYING',
