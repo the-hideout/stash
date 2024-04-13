86098d866f9ae984e2f04a14a36e418e5cd7e43d
Razzmatazz <razz@badkarma.xyz>
2024-04-13 16:28:15 -0500

    add confirmation buttons

--- commands/goons.mjs
+++ commands/goons.mjs
@@ -41,62 +41,58 @@ const defaultFunction = {
         const confirm = new ButtonBuilder()
 			.setCustomId('confirm')
 			.setLabel(t('Confirm'))
-			.setStyle(ButtonStyle.Danger);
+			.setStyle(ButtonStyle.Primary);
 
 		const cancel = new ButtonBuilder()
 			.setCustomId('cancel')
 			.setLabel(t('Cancel'))
-			.setStyle(ButtonStyle.Secondary);
+			.setStyle(ButtonStyle.Danger);
 
         const row = new ActionRowBuilder()
-			.addComponents(cancel, confirm);
+			.addComponents(confirm, cancel);
 
         const buttonResponse = await interaction.editReply({
-            content: t(`Are you sure you want to report the Goons on {{mapName}}? You will also submit your discord id# and ip address so we can act against false reports.`, {mapName: selectedMap.name}),
+            content: t(`Are you sure you want to report the Goons on {{mapName}}? You will also submit your discord id# and ip address to guard against false reports.`, {mapName: selectedMap.name}),
             components: [row],
         });
 
         const collectorFilter = i => i.user.id === interaction.user.id;
 
-        try {
-            const confirmation = await buttonResponse.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
-            if (confirmation.customId !== 'confirm') {
-                return confirmation.update({ content: t('Confirmation not received within 1 minute, cancelling'), components: [] });
-            }
+        const confirmation = await buttonResponse.awaitMessageComponent({ filter: collectorFilter, time: 60_000 }).catch(error => {
+            return false;
+        });
+        if (!confirmation) {
+            return interaction.editReply({
+                content: t('Confirmation not received within 1 minute, cancelling'),
+                components: [],
+            });
+        }
+        if (confirmation.customId !== 'confirm') {
+            return confirmation.update({ content: t('Report cancelled'), components: [] });
+        }
 
-            try {
-                const response = await got.post('https://manager.tarkov.dev/api/goons', {
-                    json: {
-                        map: selectedMap.nameId,
-                        timestamp: new Date().getTime(),
-                        accountId: parseInt(interaction.user.id.slice(-10)),
-                    },
-                }).json();
-                
-                if (response.status !== 'success') {
-                    throw new Error (response.status);
-                }
-    
-                const embed = new EmbedBuilder();
-        
-                // Construct the embed
-                embed.setTitle(t('Goons reported on {{mapName}}', {mapName: selectedMap.name}));
-                embed.setURL(`https://tarkov.dev/map/${selectedMap.normalizedName}`);
-                embed.setDescription(t('Thank you for your report!'));
-                if (selectedMap.key) {
-                    embed.setThumbnail(`https://tarkov.dev/maps/${selectedMap.key}.jpg`);
-                }
-        
-                return confirmation.update({
-                    embeds: [embed],
-                });
-            } catch (error) {
-                return confirmation.update({
-                    content: `❌ ${t('Error submitting Goons report: {{error}}', {error: error.message})}`
-                });
+        try {
+            const response = await got.post('https://manager.tarkov.dev/api/goons', {
+                json: {
+                    map: selectedMap.nameId,
+                    timestamp: new Date().getTime(),
+                    accountId: parseInt(interaction.user.id.slice(-10)),
+                },
+            }).json();
+
+            if (response.status !== 'success') {
+                throw new Error (response.status);
             }
-        } catch (e) {
-            return interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+    
+            return confirmation.update({
+                content: `✅ ${t('Thank you for your report!')}`,
+                components: [],
+            });
+        } catch (error) {
+            return confirmation.update({
+                content: `❌ ${t('Error submitting Goons report: {{error}}', {error: error.message})}`,
+                components: [],
+            });
         }
     },
     examples: [
