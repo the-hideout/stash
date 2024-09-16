716db17eeee091a0f09c5f3a29048ee16947d39e
Razzmatazz <razz@badkarma.xyz>
2024-09-16 11:39:45 -0500

    Fix /map command

--- commands/map.mjs
+++ commands/map.mjs
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 import gameData from '../modules/game-data.mjs';
-import { getFixedT, getCommandLocalizations } from '../modules/translations.mjs';
+import { getCommandLocalizations } from '../modules/translations.mjs';
 import progress from '../modules/progress-shard.mjs';
 import createEmbed from '../modules/create-embed.mjs';
 
@@ -23,8 +23,6 @@ const defaultFunction = {
     async execute(interaction) {
         await interaction.deferReply();
         const { lang, gameMode } = await progress.getInteractionSettings(interaction);
-        const t = getFixedT(lang);
-        const commandT = getFixedT(lang, 'command');
         const mapId = interaction.options.getString('map');
 
         const [maps, items] = await Promise.all([
@@ -32,7 +30,7 @@ const defaultFunction = {
             gameData.items.getAll({lang, gameMode}),
         ]);
 
-        const selectedMapData = mapData.find(mapObject => mapObject.id === mapId);
+        const selectedMapData = maps.find(mapObject => mapObject.id === mapId);
 
         const embed = await createEmbed.map(selectedMapData, interaction, {items, maps, interactionOptions: {lang, gameMode}});
 
